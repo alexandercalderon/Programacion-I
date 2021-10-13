f5e3f952c9ae5759353cdd4d4f1154b7b30c3df2
sebastiancastill <scastilloc@ucundinamarca.edu.co>
2021-10-13 16:24:20 -0500

    Castillo Sebastian

---
+++ Castillo Castillo Sebastian/EjerciciosSwitch/Ejercicio6.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class Ejercicio6 {
+
+    public static void main(String args[]) {
+
+        for (int i = 0; i <= 10000; i++)
+        {
+            System.out.println(i);
+
+        }
+    }
+
+}
