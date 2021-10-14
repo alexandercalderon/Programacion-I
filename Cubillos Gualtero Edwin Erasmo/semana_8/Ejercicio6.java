4af834f0fc2042104e48eb87a3e005420417bea5
Erasmo Cubillos <59850860+erasmocubillos@users.noreply.github.com>
2021-10-14 10:52:23 -0500

    Taller Semana 8

---
+++ Cubillos Gualtero Edwin Erasmo/semana_8/Ejercicio6.java
@@ -0,0 +1,10 @@
+package semana_8;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10000; i++){
+            System.out.println(i);
+        }
+    }
+}
