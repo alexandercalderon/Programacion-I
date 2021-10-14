a939627f171d68fb50ea86a62bc24e6b0c88c404
DanyMartin <dmartino@ucundinamarca.edu.co>
2021-10-14 10:46:01 -0500

    Daniela Martin

---
+++ Martin Ortiz Daniela/Ejercicios13102021/src/main/java/Sentenciasdecontrol/Ejercicio5.java
@@ -0,0 +1,19 @@
+
+package Sentenciasdecontrol;
+
+
+public class Ejercicio5 {
+
+    
+    public static void main(String[] args) {
+         int num = 1;
+        System.out.println("Numeros del 1 al 1000: ");                                                             
+        do{
+                   System.out.println(num);
+                   num++;
+        }while(num<=1000);
+    }
+}
+
+    
+
---
+++ Martin Ortiz Daniela/Ejercicios13102021/src/main/java/Sentenciasdecontrol/Ejercicio6.java
@@ -0,0 +1,15 @@
+
+package Sentenciasdecontrol;
+
+
+public class Ejercicio6 {
+
+    
+    public static void main(String[] args) {
+       System.out.println("Los numeros del 1 al 10000: ");                                                             
+        for(int num = 1; num<=100;num++)
+            System.out.println(num);
+    }
+}
+ 
+
