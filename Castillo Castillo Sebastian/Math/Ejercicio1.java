1267dddc23fe8b7605b33795c06ed9b06378625e
sebastiancastill <scastilloc@ucundinamarca.edu.co>
2021-10-19 11:57:57 -0500

    Castillo Sebastian

---
+++ Castillo Castillo Sebastian/Math/Ejercicio1.java
@@ -0,0 +1,13 @@
+package clasemath;
+
+public class Ejercicio1 {
+
+    public static void main(String args[]) {
+
+        int base = 5, exponente = 8;
+        double potencia = Math.pow(base, exponente);
+        System.out.println("El resultado es: " + potencia);
+
+    }
+
+}
---
+++ Castillo Castillo Sebastian/Math/Ejercicio2.java
@@ -0,0 +1,29 @@
+package clasemath;
+
+public class Ejercicio2 {
+
+    public static void main(String args[]) {
+
+        double raiz = Math.sqrt(2);
+        System.out.println(raiz);
+
+        raiz = Math.sqrt(8);
+        System.out.println(raiz);
+
+        raiz = Math.sqrt(9);
+        System.out.println(raiz);
+
+        raiz = Math.sqrt(27);
+        System.out.println(raiz);
+
+        raiz = Math.sqrt(28);
+        System.out.println(raiz);
+
+        raiz = Math.sqrt(55);
+        System.out.println(raiz);
+
+        raiz = Math.sqrt(121);
+        System.out.println(raiz);
+    }
+
+}
