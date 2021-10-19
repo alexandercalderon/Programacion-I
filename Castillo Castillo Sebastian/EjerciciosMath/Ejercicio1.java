406c1395c668e2f3887c2571b02a882969a476ca
sebastiancastill <scastilloc@ucundinamarca.edu.co>
2021-10-19 11:29:49 -0500

    Castillo Sebastian

---
+++ Castillo Castillo Sebastian/EjerciciosMath/Ejercicio1.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class Ejercicio1 {
+
+    public static void main(String args[]) {
+
+        double ValorPi = Math.PI;
+        System.out.println(ValorPi);
+
+        double raiz = Math.sqrt(25);
+        System.out.println(raiz);
+    }
+
+}
