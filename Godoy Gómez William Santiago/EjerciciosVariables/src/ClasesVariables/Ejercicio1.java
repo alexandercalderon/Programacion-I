af12fcb4d20cfb5ebf9ffcdf3b7e2e463aa1c35c
Wisan0346 <89869338+Wisan0346@users.noreply.github.com>
2021-10-23 13:23:49 -0500

    Add files via upload

---
+++ Godoy Gómez William Santiago/EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,17 @@
+
+package ClasesVariables;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+      String Nombre = "Michael";
+        double x = 3.25;
+        double dolar = 3851.22;
+        float decimal = 3.1415926f; 
+        double Euler = 2.718281828459045;
+        System.out.println(Nombre);
+        System.out.println(x);
+        System.out.println(dolar);
+        System.out.println(decimal);
+        System.out.println(Euler);
+    }
+    
+}
---
+++ Godoy Gómez William Santiago/EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,38 @@
+
+package ClasesVariables;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+       int a = 8; 
+       char b = 'A'; 
+       String c = "Colombia"; 
+       char d = 'A'; 
+       float e = 74548f; 
+       int numero = 1; 
+       String nombrePersona = "Nuria"; 
+       int edad = 18;
+       String altura = "1.75";
+       double alturaR = Double.parseDouble(altura);
+       int doble = 96; 
+       int A = 98; 
+       char x = 'a'; 
+       int y = 25; 
+       char g = 'y'; 
+        
+       System.out.println(+a); 
+       System.out.println("La variable b es: " +b); 
+       System.out.println(c+ " es un pais muy bonito");      
+       System.out.println("La variable d es: " +d); 
+       System.out.println(+e); 
+       System.out.println(+numero); 
+       System.out.println(""+nombrePersona); 
+       System.out.println("Tengo "+edad+ " años"); 
+       System.out.println("Mido "+alturaR+ " cm"); 
+       System.out.println(+doble); 
+       System.out.println(+A); 
+       System.out.println("La variable x es: "+x); 
+       System.out.println(+y); 
+       System.out.println("La variable g es: "+g);
+       
+    }
+    
+}
